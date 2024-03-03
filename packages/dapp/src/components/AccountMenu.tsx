99b63d4b9f0f1f09e8279abc3db06e74cccb5aca
0xMemoryGrinder <35138272+0xMemoryGrinder@users.noreply.github.com>
2024-03-03 15:55:36 -0500

    feat(snap): added showCredentials button in account selection menu

--- packages/dapp/src/components/AccountMenu.tsx
+++ packages/dapp/src/components/AccountMenu.tsx
@@ -1,5 +1,5 @@
 import { AddIcon, ChevronDownIcon, DownloadIcon } from '@chakra-ui/icons';
-import { Menu, MenuButton, MenuList, MenuItem, Button, Flex, useDisclosure, Spinner } from '@chakra-ui/react';
+import { Menu, MenuButton, MenuList, MenuItem, Button, Flex, useDisclosure, Spinner, Text, IconButton, useColorModeValue, Box } from '@chakra-ui/react';
 import { useState } from 'react';
 import { ImportAccountModal } from './ImportAccountModal';
 import { GenerateAccountModal } from './GenerateAccountModal';
@@ -7,14 +7,17 @@ import { useAccountList } from '@/hooks/useAccountList';
 import { useActiveAccount, invalidateActiveAccount } from '@/hooks/useActiveAccount';
 import { useSetActiveAccount } from '@/hooks/useSetActiveAccount';
 import { invalidateOperations } from '@/hooks/useOperations';
+import { useShowCredentials } from '@/hooks/useShowCredentials';
 
 export const AccountMenu = () => {
   const { isOpen: isImportOpen, onOpen: onImportOpen, onClose: onImportClose} = useDisclosure();
   const { isOpen: isGenerateOpen, onOpen: onGenerateOpen, onClose: onGenerateClose} = useDisclosure();
+  const showIconBg = useColorModeValue('gray.100', 'gray.600');
 
   const { isLoading: accountsListLoading, data: accountsList } = useAccountList()
   const { isLoading: activeAccountLoading, data: activeAccount } = useActiveAccount();
   const setActiveAccount = useSetActiveAccount();
+  const showCredentials = useShowCredentials();
 
   return (
     <Menu>
@@ -25,12 +28,20 @@ export const AccountMenu = () => {
         {!accountsListLoading && accountsList?.map((account) => (
           <MenuItem
             key={account!.address}
-            onClick={() => setActiveAccount({ address: account!.address })?.then(() =>  {
-              invalidateActiveAccount();
-              invalidateOperations();
-            })}
           >
-            {account!.name}
+            <Flex justify={'space-between'} align={"center"} w={'full'}>
+              <Text flexGrow={1}
+                onClick={() => setActiveAccount({ address: account!.address })?.then(() =>  {
+                  invalidateActiveAccount();
+                  invalidateOperations();
+                })}
+              >{account!.name}</Text>
+              <Box borderRadius={'lg'} bg={showIconBg} p={2}>
+                <DownloadIcon  onClick={
+                  () => showCredentials({address: account!.address})
+                }/>
+              </Box>
+            </Flex>
           </MenuItem>
         ))}
         <Flex justifyContent={'space-between'} m={4} gap={4}>
